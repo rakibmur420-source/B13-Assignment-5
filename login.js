103511ae25db1f69f3da9c50ed522f630fc6f6df
rakib <rakibmur420@gmail.com>
2026-03-06 23:58:40 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,23 @@
+
+
+document.getElementById("login-btn").addEventListener("click", function(){
+// get text input
+const textInput = document.getElementById("input-text");
+const name = textInput.value;
+console.log(name);
+
+// get pin input
+const inputPin= document.getElementById("input-password");
+const pin=inputPin.value
+console.log(pin);
+//match  pin & num
+if(name=="admin" && pin=="admin123"){
+    alert("login Success");
+    window.location.assign("/home.html")
+}else{
+    alert("login Failed");
+    return;
+}
+
+});
+
